4bd5a57f7e4053da06bd942f85cf74e0edb3e2ee
Fu <sunyangfu@gmail.com>
2020-04-24 15:55:52 -0500

    kp refinement v2, fixed context

--- SBI/model/run_output_sbi.py
+++ SBI/model/run_output_sbi.py
@@ -24,6 +24,13 @@ def rad_parser(line):
 	line = line_str
 	return line
 
+def read_txt(indir):
+	f = open(indir,'r')
+	txt = f.read()
+	f.close()
+	l = txt.split('\n')
+	return l	
+
 def read_file_list(indir, deli):
 	opt_notes = []
 	with open(indir, 'rU') as csvfile:
@@ -33,13 +40,31 @@ def read_file_list(indir, deli):
 	return opt_notes
 
 def apply_exclusion_wmd(sent):
-	elist = ['lacunar', 'gliotic type signal', 'edematous', 'probably', 'may represent', 'multiple sclerosis', 'cervical spine', 'demyelinating plaque', 'joints', 'indeterminate', 'could represent', 'spine']
+	elist = read_txt('SBI/regexp/resources_regexp_reWMDCXEXL.txt')
 	for k in elist:
 		k = ' '+k+' '
 		sent = ' '+sent.lower().replace('.', ' ')
 		if k in sent:
 			return True
 	return False
+
+def apply_positive_screen(sent):
+	elist = read_txt('SBI/regexp/resources_regexp_reSBIAlone.txt')
+	for k in elist:
+		k = ' '+k+' '
+		sent = ' '+sent.lower().replace('.', ' ')
+		if k in sent:
+			return True
+	return False	
+
+def apply_negative_screen(sent):
+	elist = read_txt('SBI/regexp/resources_regexp_reSBICXEXL.txt')
+	for k in elist:
+		k = ' '+k+' '
+		sent = ' '+sent.lower().replace('.', ' ')
+		if k in sent:
+			return False
+	return True
 
 def get_remove(l, deli):
 	r = {}
@@ -59,34 +84,60 @@ def run_eval_sbi(indir, outdir, sys):
 		deli = '/'
 	else:
 		deli = '\\'
-
 	dir_list = indir+deli+'*.ann'
 	l = glob.glob(dir_list)
 	output = []
 	rm = get_remove(l, deli)
-
-	# print (rm)
 
 	with open(outdir+deli+'patient_level.csv', 'w') as csvfile:
 		spamwriter = csv.writer(csvfile, delimiter='|')
 		for d in l:
 			fname = d.split(deli)[-1]
 			SBI_STATUS, WMD_STATUS, WMD_GD = '','',''
+			SBI_STATUS_LIST = []
 			ann_list = read_file_list(d,'\t')
 			for row in ann_list:
+				rm_term = fname+row[-2]
 				certainty = row[6]
 				status = row[7]
 				exp = row[8]
 				norm = row[9]
-				sent = row[-1]
+				sent = row[-1].lower()
 				key = fname+row[-2]
-				if ('Positive' in certainty) and 'Present' in status and 'Patient' in exp:
-					if ('NONACUTE' in norm or 'INF_GENERAL' in norm or 'ACUTE' in norm) and key not in rm:
-						SBI_STATUS = 'SBI_FOUND'
-					if 'WMD_WHITE' in norm or 'WMD_LEUK' in norm or 'WMD' in norm:
+				double_eval = False
+				indeterminate_status = False
+				# if ('Negation' not in certainty) and 'Present' in status and 'Patient' in exp:
+				# 	if'INF_INDETERMINATE' in norm:
+				# 		SBI_STATUS_LIST += ['SBI_INDETERMINATE']
+				# 		indeterminate_status = True
+				# if ('Possible' in certainty) and 'Present' in status and 'Patient' in exp:
+				# 	if ('NONACUTE' in norm or 'INF_GENERAL' in norm or 'ACUTE' in norm):
+				# 		SBI_STATUS_LIST += ['SBI_INDETERMINATE']
+				# 		indeterminate_status = True
+				# if ('Hypothetical' in certainty) and 'Present' in status and 'Patient' in exp:
+				# 	if ('NONACUTE' in norm or 'INF_GENERAL' in norm or 'ACUTE' in norm):
+				# 		SBI_STATUS_LIST += ['SBI_INDETERMINATE']
+				# 		indeterminate_status = True
+				if indeterminate_status:
+					ps = apply_positive_screen(sent)
+					if ps:
+						SBI_STATUS_LIST += ['SBI_FOUND']
+				if ('Positive' in certainty):
+					if ('NONACUTE' in norm or 'INF_GENERAL' in norm or 'ACUTE' in norm):
+						ns = apply_negative_screen(sent)
+						if ns:
+							SBI_STATUS_LIST += ['SBI_FOUND']
+						# else:
+						# 	SBI_STATUS_LIST += ['SBI_INDETERMINATE']
+				if ('Negated' not in certainty):							
+					if 'WMD_WHITE' in norm or 'WMD_LEUK' in norm or 'WMD' in norm or 'WMD_SV' in norm:
 						exl = apply_exclusion_wmd(sent)
-						if not exl:
+						if not exl and rm_term not in rm:
 							WMD_STATUS = 'WMD_FOUND'
+			if 'SBI_FOUND' in SBI_STATUS_LIST:
+				SBI_STATUS = 'SBI_FOUND'
+			# elif 'SBI_INDETERMINATE' in SBI_STATUS_LIST:
+			# 	SBI_STATUS = 'SBI_INDETERMINATE'
 			for row in ann_list:
 				if WMD_STATUS == 'WMD_FOUND':
 					if 'SEVERE' in norm:
--- runMedTagger-fit-sbi.sh
+++ runMedTagger-fit-sbi.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
-INPUT_DIR="/ESPRESSO_NLP/input"
-OUTPUT_DIR="/ESPRESSO_NLP/output/raw"
-OUTPUT_SUMMARY_DIR="/ESPRESSO_NLP/output/summary"
-RULES_DIR="/ESPRESSO_NLP/SBI"
+INPUT_DIR="/Users/m176320/Documents/a_projects/LongTerm/SBI/data/raw_report/"
+# INPUT_DIR="/Users/m176320/Documents/workspace3/ESPRESSO_NLP/input/"
 
-java -Xms512M -Xmx2000M -jar MedTagger-fit-1.0.2-SNAPSHOT.jar $INPUT_DIR $OUTPUT_DIR $RULES_DIR
+OUTPUT_DIR="/Users/m176320/Documents/workspace3/ESPRESSO_NLP/output/raw"
+OUTPUT_SUMMARY_DIR="/Users/m176320/Documents/workspace3/ESPRESSO_NLP/output/summary"
+RULES_DIR="/Users/m176320/Documents/workspace3/ESPRESSO_NLP/SBI"
+
+java -Xms512M -Xmx2000M -jar MedTagger-fit.jar $INPUT_DIR $OUTPUT_DIR $RULES_DIR
 
 python SBI/model/run_output_sbi.py $OUTPUT_DIR $OUTPUT_SUMMARY_DIR "0"
